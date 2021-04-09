75d168f44743fdaaef9823aa75e961884fec79fb
imaxfp@gmail.com <imaxfp@gmail.com>
2021-04-09 21:56:59 +0300

    [None] implemented solution for dynamic matrix

--- src/topic2/word_to_vector.py
+++ src/topic2/word_to_vector.py
@@ -9,56 +9,76 @@ similarity and simple linear algebra
 
 @see https://www.baeldung.com/cs/convert-word-to-vector
 
-TFFDF
-PMI
-Word Embeddings (CBO)
 """
 
-# Do not change the sentences
-sentence1 = "Socrates likes to watch movies. Mary likes movies too."
-sentence2 = "Rene Descartes likes to watch movies. Mary likes movies too."
-sentence3 = "Kant also likes to watch football games"
 
-
-def collect_all_unique_words(list_of_sentences):
+def collect_all_unique_words(list_of_words):
     """
     1. all unique words have to be collected to the list
     """
-    # TODO 1 use HOF 'map, filter, etc'
-    return ['a', 'b', 'c']
+    # example 1
+    # unique_words = list(set(list_of_sentences))
+
+    # example 2
+    unique_words = list()
+    for w in list_of_words:
+        if unique_words.count(w) == 0:
+            unique_words.append(w)
+
+    unique_words.sort(reverse=False)
+    return unique_words
 
 
-def to_lowercase_and_remove_stop_words(list_of_sentences):
+def to_lowercase_and_remove_stop_words(list_of_words, stop_words_list):
     """
     1. Uppercase should be transform to lower case. For example 'Hello' -> 'hello'
-    2. Remove all STOP WORDS like - 'to, and, etc' Create you own list
+    2. Remove all STOP WORDS like - 'too, and, etc'
 
     """
-    # TODO 2
-    return None
+    res = list(filter(lambda x: stop_words_list.count(x) == 0, list_of_words))
+    res = list(map(lambda x: x.lower(), res))
+    return res
 
 
-def create_matrix(list_of_sentences, count_of_columns):
-    """
-    type of the matrix will be 'int'
-    Title of the matrix == our unique words
+class matrixObject():
 
-    TITLE---------------------'movies'--'watch'--'football'--'...'
-    3 line of the matrix 'int''   1  '--'  1  '--'   0   '--'...'
-                              '   1  '--'  1  '--'   0   '--'...'
-                              '   0  '--'  1  '--'   1   '--'...'
-    """
-    # TODO 3 collect all unique words from the all sentences
-    return None
+    def __init__(self, count_of_columns, stop_words_list=None, matrix_title_list=None):
+        if stop_words_list is None: stop_words_list = []
+        if matrix_title_list is None: matrix_title_list = []
+        self.count_of_columns = count_of_columns
+        self.stop_words_list = stop_words_list
+        self.matrix_title_list = matrix_title_list
+        self.vector_matrix = []
+
+    def update_title(self, list_of_words):
+        res = collect_all_unique_words(list_of_words)
+        res = to_lowercase_and_remove_stop_words(res, self.stop_words_list)
+        return res
+
+    def extend_matrix(self, list_of_words):
+        res = self.update_title(list_of_words)
+        self.matrix_title_list = collect_all_unique_words(res + self.matrix_title_list)
+        self.vector_matrix.append(list(map(lambda w: res.count(w), self.matrix_title_list)))
+
+    def print_matrix(self):
+        print(self.matrix_title_list)
+        list(map(lambda w: print(w), self.vector_matrix))
 
 
 if __name__ == '__main__':
+
+    list_for_matrix = ["Socrates Socrates likes to watch movies. Mary likes movies too.",
+                       "Rene Descartes likes to watch movies. Mary likes movies too.",
+                       "Kant, also likes to watch football games",
+                       "Test",
+                       "Hello"]
+
     '''
     we need to deal with linguistic entities such as words?
     How can we model them as mathematical representations? The answer is we convert them to vectors!
     '''
+    matrix = matrixObject(count_of_columns=10)
+    for i in list_for_matrix:
+        matrix.extend_matrix(i.split())
 
-    # TODO 3 create the matrix where count of COLUMNS is unique words and ROWS count of sentences
-
-    # https://www.baeldung.com/cs/convert-word-to-vector#one-hot-vectors
-    # vector where each column corresponds to a word
+    matrix.print_matrix()
--- test/topic2/word_to_vector.py
+++ test/topic2/word_to_vector.py
@@ -1,14 +1,19 @@
 import unittest
-from src.topic1.lesson3.loops import loops_task1, loops_task2
-from src.topic2.word_to_vector import collect_all_unique_words
+from src.topic2.word_to_vector import collect_all_unique_words, to_lowercase_and_remove_stop_words
 
 
 class MyTestCase(unittest.TestCase):
 
-    def collect_all_unique_words_task1(self):
-        self.assertTrue(collect_all_unique_words(['a', 'b', 'c', 'a', 'a', 'c']), ['a', 'b', 'c'])
+    def test_collect_all_unique_words(self):
+        self.assertEqual(collect_all_unique_words(['a', 'b', 'c', 'a', 'a', 'c']), ['a', 'b', 'c'])
 
-    # TODO please cover all functions
+    def test_to_lowercase_and_remove_stop_words(self):
+        self.assertEqual(to_lowercase_and_remove_stop_words(['Hello', 'ABC', 'to', 'and']), ['hello', 'abc'])
+
+    def test_lowercase_and_remove_stop_words(self):
+        stop_words_list = ["to", "and"]
+        self.assertEqual(to_lowercase_and_remove_stop_words(['Hello', 'ABC', 'to', 'and'], stop_words_list),
+                         ['hello', 'abc'])
 
 
 if __name__ == '__main__':
