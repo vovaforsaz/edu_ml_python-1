c2ba94c6ad6bf49921c76d051513c39aa46c01d4
ab260791tvn <vovaforsaz@gmail.com>
2021-04-12 06:55:19 +0300

    ttest

--- src/topic2/word_to_vector.py
+++ src/topic2/word_to_vector.py
@@ -16,15 +16,15 @@ def collect_all_unique_words(list_of_words):
     1. all unique words have to be collected to the list
     """
     # example 1
-    # unique_words = list(set(list_of_sentences))
+    unique_words = list(set(list_of_words))
 
     # example 2
-    unique_words = list()
-    for w in list_of_words:
-        if unique_words.count(w) == 0:
-            unique_words.append(w)
-
-    unique_words.sort(reverse=False)
+    # unique_words = list()
+    # for w in list_of_words:
+    #     if unique_words.count(w) == 0:
+    #         unique_words.append(w)
+    #
+    # unique_words.sort(reverse=False)
 
     # example 3 please use HOF 'map, filter, zip'
     return unique_words
@@ -40,7 +40,10 @@ def to_lowercase_and_remove_stop_words(list_of_words, stop_words_list):
     res = list(map(lambda x: x.lower(), res))
     return res
 
-
+# def count_fun(w):
+#     for i in w:
+#         matrix.extend_matrix(i.split())
+#     res.count(i)
 class matrixObject():
 
     def __init__(self, count_of_columns, stop_words_list=None, matrix_title_list=None):
@@ -59,14 +62,14 @@ class matrixObject():
     def extend_matrix(self, list_of_words):
         res = self.update_title(list_of_words)
         self.matrix_title_list = collect_all_unique_words(res + self.matrix_title_list)
-        self.vector_matrix.append(list(map(lambda w: res.count(w), self.matrix_title_list)))
+        self.vector_matrix.append(list(map(lambda w: print(res.count(w)), self.matrix_title_list)))
 
     def print_matrix(self):
         print(self.matrix_title_list)
         list(map(lambda w: print(w), self.vector_matrix))
-
-    def get_title(self):
-        return self.matrix_title_list
+    #
+    # def get_title(self):
+    #     return self.matrix_title_list
 
 
 if __name__ == '__main__':
@@ -75,13 +78,14 @@ if __name__ == '__main__':
                        "Rene Descartes likes to watch movies. Mary likes movies too.",
                        "Kant, also likes to watch football games",
                        "Test",
+                       "Kant, also likessada to waasdtch asdasfootball games",
                        "Hello"]
 
     '''
     We need to deal with linguistic entities such as words?
     How can we model them as mathematical representations? The answer is we convert them to vectors!
     '''
-    matrix = matrixObject(count_of_columns=10)
+    matrix = matrixObject(count_of_columns=5)
     for i in list_for_matrix:
         matrix.extend_matrix(i.split())
 
